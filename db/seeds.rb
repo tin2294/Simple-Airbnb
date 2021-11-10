970a996f330333e0ab34786aa842f36b7f466a6f
Ting Ting Chen <tin2294@gmail.com>
2021-11-10 16:56:19 +0100

    simple airbnb basics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & Spacious Penthouse Rainbowland',
+  address: '10 Rainbow Ave',
+  description: 'A lovely rainbowy feel for this spacious penthouse. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 175,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Gloomy & Tiny Studio Gloomyland',
+  address: '10 Gloomy Ave',
+  description: 'A lovely creepy feel for this tiny studio.',
+  price_per_night: 20,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'MARVELous Manor',
+  address: '10 Marvel Ave',
+  description: 'A lovely MARVEL experience at this huge and quite heroic manor.',
+  price_per_night: 2000,
+  number_of_guests: 15
+)
